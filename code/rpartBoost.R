8d8771c6953ba2ca1c5760d1204cef37f7492734
bgreenwell <greenwell.brandon@gmail.com>
2018-02-05 13:12:35 -0500

    improve boosting gif

--- code/rpartBoost.R
+++ code/rpartBoost.R
@@ -1,3 +1,6 @@
+cols <- RColorBrewer::brewer.pal(9, "Set1")
+
+# Function to implement gradient boosting with squared-error loss
 rpartBoost <- function(X, y, data, num_trees = 100, learn_rate = 0.1, 
                        tree_depth = 6, verbose = FALSE) {
   require(rpart)
@@ -16,9 +19,13 @@ rpartBoost <- function(X, y, data, num_trees = 100, learn_rate = 0.1,
   G_b_hat
 }
 
+# Function to plot the predictions from a particular boosting iteration
 plotIter <- function(object, iter, ...) {
   plot(x, y)
-  lines(x, object[, iter + 1], lwd = 3, ...)
+  lines(x, sin(x), lwd = 3, col = cols[2L])
+  lines(x, object[, iter + 1], lwd = 3, col = cols[1L], ...)
+  legend("topright", legend = c("Boosted prediction", "True function"),
+         lty = 1L, lwd = 3L, col = cols[1L:2L], inset = 0.01)
 }
 
 # Simulate some sine wave data
@@ -28,10 +35,20 @@ y <- sin(x) + rnorm(length(x), sd = 0.3)
 plot(x, y)
 
 # gradient boosted decision trees
-bst <- rpartBoost(X = x, y = y, num_trees = 500, learn_rate = 0.5, 
+bst <- rpartBoost(X = x, y = y, num_trees = 100, learn_rate = 0.5, 
                   tree_depth = 1, verbose = TRUE)
 
+# Plot first 15 iterations
 par(mfrow = c(4, 4))
 for (i in 0:15) {
-  plotIter(bst, iter = i, col = "red2")
+  plotIter(bst, iter = i)
 }
+
+# Make a gif (requires that ImageMagick beinstalled on machine)
+png(file = "gifs/boosted-stumps%02d.png", width = 500, height = 500)
+for (i in c(0:100)){
+  plotIter(bst, iter = i)
+}
+dev.off()
+system("convert -delay 30 gifs/*.png gifs/boosted_stumps.gif")
+file.remove(list.files(path = "gifs", pattern = ".png", full.names = TRUE))
